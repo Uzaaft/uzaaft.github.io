b44a4db8e4cddde3e2b739f981d51e3701baa3f3
Uzair Aftab <git@uzaaft.me>
2025-11-26 09:37:48 +0100

    chore: setup nix flake with dummy astro code and pnpm

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
   };
 
@@ -12,15 +12,42 @@
     eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
   in {
     packages = eachSystem (pkgs: {
-      hello = pkgs.hello;
+      default = pkgs.stdenv.mkDerivation {
+        pname = "uzaaft-github-io";
+        version = "0.0.1";
+        src = ./.;
+
+        nativeBuildInputs = [
+          pkgs.nodejs
+          pkgs.pnpm.configHook
+        ];
+
+        pnpmDeps = pkgs.pnpm.fetchDeps {
+          pname = "uzaaft-github-io-pnpm-deps";
+          src = ./.;
+          lockfile = ./pnpm-lock.yaml;
+          fetcherVersion = 2;
+          hash = "sha256-PjHxlpHr1OxB3BKV7jhIyWSTzojlsjvzFTYEHg/+nB4=";
+        };
+
+        buildPhase = "pnpm build";
+        installPhase = "mv dist $out";
+      };
     });
 
     devShells = eachSystem (pkgs: {
       default = pkgs.mkShell {
         buildInputs = with pkgs; [
+          nodejs
+          pnpm
           pandoc
           texliveFull
+          astro-language-server
         ];
+
+        shellHook = ''
+          export PATH=$PWD/node_modules/.bin:$PATH
+        '';
       };
     });
   };
