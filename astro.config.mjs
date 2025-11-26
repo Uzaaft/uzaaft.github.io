3469323477aab927fc19dcb4a8aa189fd1cd2f77
Uzair Aftab <git@uzaaft.me>
2025-11-26 11:41:25 +0100

    build: make it deployable
    fix: update dependencies and fix commands

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import markdoc from '@astrojs/markdoc';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [markdoc()],
+  site: 'https://uzaaft.me',
+  integrations: [
+    markdoc(),
+    sitemap()
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
